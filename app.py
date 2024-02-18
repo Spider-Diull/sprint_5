a673361931fb998eea1d6d6a4749eeeaffe02e11
Spider-Diull <diego.ulloa1360@gmail.com>
2024-02-18 17:48:25 -0300

    Encabezado agregado

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import pandas
 import plotly_express
 
 car_data=pandas.read_csv('vehicles_us.csv') # Leimos los datos del CSV
-hist_button = streamlit.button('Construir histograma') # crear un botón
+streamlit.header('Graficos de anuncios de venta de vehiculos')
 #BOTON PARA CONSTRUIR HISTOGRAMA
+hist_button = streamlit.button('Construir histograma') # crear un botón
 if hist_button: # al hacer clic en el botón
     streamlit.write('Creación de un histograma para el conjunto de datos de anuncios de venta de vehiculos')# escribir un mensaje
     hist_graphic = plotly_express.histogram(car_data, x="odometer")# crear un histograma
     streamlit.plotly_chart(hist_graphic, use_container_width=True)# mostrar un gráfico Plotly interactivo
-    
+
 #BOTON PARA CONSTRUIR GRAFICO DE DISPERSION
 scatter_chekbox = streamlit.checkbox('Costruir grafico de dispersion') # crear una casilla de verificación
 if scatter_chekbox:
