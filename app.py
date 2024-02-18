76c0306eed023bc0a9a09d94c5a448adbc05d331
Spider-Diull <diego.ulloa1360@gmail.com>
2024-02-18 17:01:04 -0300

    Buttons hist and scatter

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit
+import pandas
+import plotly_express
+
+car_data=pandas.read_csv('vehicles_us.csv') # Leimos los datos del CSV
+hist_button = streamlit.button('Construir histograma') # crear un botón
+#BOTON PARA CONSTRUIR HISTOGRAMA
+
+
+#build_histogram = st.checkbox('Construir un histograma') # crear una casilla de verificación
+
+if hist_button: # al hacer clic en el botón
+    streamlit.write('Creación de un histograma para el conjunto de datos de anuncios de venta de vehiculos')# escribir un mensaje
+    
+    hist_graphic = plotly_express.histogram(car_data, x="odometer")# crear un histograma
+    
+    streamlit.plotly_chart(hist_graphic, use_container_width=True)# mostrar un gráfico Plotly interactivo
+#BOTON PARA CONSTRUIR GRAFICO DE DISPERSION
+scatter_button = streamlit.button('Costruir grafico de dispersion')
+if scatter_button:
+    streamlit.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de vehiculos')# escribir mensaje
+    scatter_graphic=plotly_express.scatter(car_data,x='odometer',y='price')# crear grafico de dispersion
+    streamlit.plotly_chart(scatter_graphic,use_container_width=True)# mostrar un gráfico Plotly interactivo
